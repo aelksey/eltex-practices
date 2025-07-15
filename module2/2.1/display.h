99b4a83fc0375640f5ed2afe752cb6d8fd6948ac
dukealys <dukealys@student.21-school.ru>
2025-07-15 23:14:10 +0700

    started work on module 2 - practice 1 - task 2

---
+++ module2/2.1/display.h
@@ -0,0 +1,16 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "contact.h"
+
+void display_contact(contact_t contact);
+
+void display_contact_fio(contact_t contact);
+
+void display_contact_job(contact_t contact);
+
+void display_contact_phone(contact_t contact);
+
+void display_contact_socials(contact_t contact);
+
+#endif 
