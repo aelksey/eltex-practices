7b14605d045c0e0c098a1287c6141e1e0e93a8e0
dukealys <dukealys@student.21-school.ru>
2025-07-29 12:30:05 +0700

    module3 started : 01 completed

---
+++ module3/01/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+#define STRING 0
+#define INTEGER 1
+#define DOUBLE 2
+
+int arg_type(const char *arg);
+
+void handle_arg(int proc_selector ,const char *arg);
+
+int main(int argc,char *argv[]) {
+    
+    int proc_selector = 0; // 0 - parent ; 1 - child
+
+    for(int i = 1; i < argc;i++){
+        handle_arg(proc_selector,argv[i]);
+        // Change proc selector to child process
+        if(proc_selector == 0)proc_selector = 1;
+        // Change proc selector to parent process
+        if(proc_selector == 1)proc_selector = 0;
+    }
+
+    return 0;
+}
+
+int arg_type(const char *arg){
+    int type = STRING;
+    char *endptr;
+    if(atoi(arg) != 0)type = INTEGER;
+    if(atof(arg) != 0)type = DOUBLE; 
+    return type;
+}
+
+void handle_arg(int proc_selector ,const char *arg){
+    if(proc_selector == 0){
+        char *endptr;
+        if (arg_type(arg) == STRING)printf(" %s ",arg);
+        if (arg_type(arg) == INTEGER)printf(" %d %d ",atoi(arg),atoi(arg)*2);
+        if (arg_type(arg) == DOUBLE)printf(" %.2lf %.2lf ",strtod(arg,&endptr),strtod(arg,&endptr)*2);
+    }
+
+    if(proc_selector == 1){
+        pid_t pid = fork();
+        if(pid == 0){
+            char *endptr;
+            if (arg_type(arg) == STRING)printf(" %s ",arg);
+            if (arg_type(arg) == INTEGER)printf(" %d %d ",atoi(arg),atoi(arg)*2);
+            if (arg_type(arg) == DOUBLE)printf(" %.2lf %.2lf ",strtod(arg,&endptr),strtod(arg,&endptr)*2);
+            _exit(EXIT_SUCCESS);
+        }
+    }
+}
+
